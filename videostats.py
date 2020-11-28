9a6b573f41ea725350a095fa27cabb03a869fb32
Darin Pesnell <63257529+dpesnell@users.noreply.github.com>
2020-11-28 13:36:32 -0500

    Update videostats.py

--- videostats.py
+++ videostats.py
@@ -7,5 +7,5 @@ while x == 0:
     stream = os.popen('netstat | grep 1935')
     output = stream.read()
     if "1935" in output:
-        requests.get("https://s7fv6gymxa.execute-api.us-east-1.amazonaws.com/default/record_video_sessions")
+        requests.get("https://XXXXXXXX.execute-api.us-east-1.amazonaws.com/default/record_video_sessions")
     time.sleep(10)
